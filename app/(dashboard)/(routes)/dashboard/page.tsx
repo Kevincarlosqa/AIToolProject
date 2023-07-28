80b904296b55c287d38d055ae882c612841eea8f
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-27 21:52:10 -0500

    add: Auth w/Clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <>
       <p className="text-6xl text-green-600">DashBoard(protected)</p>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 };
