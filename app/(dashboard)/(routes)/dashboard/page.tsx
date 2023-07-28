78b026d4d4b5d6b7abb11abcc96816e4f7553780
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-27 21:06:00 -0500

    add: Setup Pages

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <p className="text-6xl text-green-600">DashBoard(protected)</p>
+    </>
+  );
+};
+
+export default DashboardPage;
