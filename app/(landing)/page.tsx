2a3fcad0d01e8e41668980a93e3ac5c7022a537e
Kevin Quispe <kevincarlosqa@gmail.com>
2023-08-04 09:51:04 -0500

    add: landing navbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,18 +1,11 @@
+import { LandingNavbar } from "@/components/landing-navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page (Unprotected)
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Sign Up</Button>
-        </Link>
-      </div>
+    <div className="h-full">
+      <LandingNavbar />
     </div>
   );
 };
