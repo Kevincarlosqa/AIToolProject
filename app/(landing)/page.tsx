e14901e1253db31957578d5957c028d21706fcf8
Kevin Quispe <kevincarlosqa@gmail.com>
2023-08-04 10:23:39 -0500

    add: landing content

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,13 @@
+import LandingContent from "@/components/landing-content";
 import LandingHero from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
+      <LandingContent />
     </div>
   );
 };
